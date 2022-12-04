9bfb281bb87df75bcf33f92230d72711171ef5dd
Wyatt Marvil <2020wmarvil@gmail.com>
2022-12-03 20:03:59 -0500

    Procedural Texture

--- source/main.cpp
+++ source/main.cpp
@@ -3,10 +3,33 @@
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
 
+#include "shader.h"
+
 void ProcessInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+}
+
+GLenum glCheckError()
+{
+    GLenum errorCode;
+    while ((errorCode = glGetError()) != GL_NO_ERROR)
+    {
+        std::string error;
+        switch (errorCode)
+        {
+            case GL_INVALID_ENUM:                  error = "INVALID_ENUM"; break;
+            case GL_INVALID_VALUE:                 error = "INVALID_VALUE"; break;
+            case GL_INVALID_OPERATION:             error = "INVALID_OPERATION"; break;
+            case GL_STACK_OVERFLOW:                error = "STACK_OVERFLOW"; break;
+            case GL_STACK_UNDERFLOW:               error = "STACK_UNDERFLOW"; break;
+            case GL_OUT_OF_MEMORY:                 error = "OUT_OF_MEMORY"; break;
+            case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
+        }
+        std::cout << error << std::endl;
+    }
+    return errorCode;
 }
 
 int main()
@@ -32,25 +55,103 @@ int main()
         return -1;
     }
 
-    // Handle view port dimensions
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height)
     {
         glViewport(0, 0, width, height);
     });
 
-    // This is the render loop
+    float vertices[] = {
+         -1.f, -1.f, 0.f, 0.f, 0.f,
+         -1.f,  1.f, 0.f, 0.f, 1.f,
+          1.f, -1.f, 0.f, 1.f, 0.f,
+          1.f,  1.f, 0.f, 1.f, 1.f,
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        1, 2, 3,
+    };
+
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // uv attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    Shader shader("../../source/shaders/quad.vert", "../../source/shaders/quad.frag");
+    shader.use();
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    struct Color {
+        unsigned char r;
+        unsigned char g;
+        unsigned char b;
+        unsigned char a;
+    };
+
+    struct Image {
+        int width;
+        int height;
+        Color *data;
+
+        Image(int width, int height)
+        {
+            this->width = width;
+            this->height = height;
+            this->data = (Color*)calloc(width * height, sizeof(Color));
+        }
+    };
+
+    Image image(5, 5);
+    for (int index = 0; index < image.width * image.height; index++)
+    {
+        image.data[index] = { (unsigned char)(index / (1.f + image.width*image.height) * 255), 0, 0 };
+    }
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image.data);
+    glCheckError();
+    glGenerateMipmap(GL_TEXTURE_2D);
+
     while (!glfwWindowShouldClose(window))
     {
         ProcessInput(window);
 
-        // Druids are the best
         glClearColor(1.00f, 0.49f, 0.04f, 1.00f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
